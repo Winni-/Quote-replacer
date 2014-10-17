a46c30bd5779dad1b345c67211ac56a7d4dc96ee
Mihail Pavlov <winni_ne@mail.ru>
2014-10-17 13:25:53 +0400

    Added <script> support

--- quotes.js
+++ quotes.js
@@ -4,16 +4,25 @@ $(function() {
 	var text = $(target).html();
 	var weAreInTag = false;// Means we are beetwen < and >
 	var weHaveOpened = false;
+	var script = false; // We do not want to do anything beetwen <script> and </script>
 	var newText = [];
 	for (var i = 0; i <= text.length - 1; i++) {	
 		if (weAreInTag) {
 			if (text[i] == ">") {
-				weAreInTag = false;				
+				weAreInTag = false;	
+				var ipt = ""+text[i-1]+text[i-2]+text[i-3];
+				if ( scr == "scr" || scr == "SCR") {
+					script = false;
+				};			
 			};			
 		}else{
 			if (text[i] == "<") {
-				weAreInTag = true;				
-			}else if (text[i]=='"'||text[i]=="'") {
+				weAreInTag = true;
+				var scr = ""+text[i+1]+text[i+2]+text[i+3];
+				if ( scr == "scr" || scr == "SCR") {
+					script = true;
+				};				
+			}else if ( (text[i]=='"'||text[i]=="'") && !script) {
 				if (weHaveOpened) {
 					newText.push("»");
 					weHaveOpened = false;
@@ -28,6 +37,6 @@ $(function() {
 		newText.push(text[i]);	
 	};
 	text = newText.join("");	
-	$(target).empty().append(text);
+	$(target).empty().html(text);
 	$(document).trigger("ready");
 });
